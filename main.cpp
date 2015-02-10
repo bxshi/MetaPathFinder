b35509499952ab4fd007352092dca423d4bdaf9c
bxshi <baoxu.shi@gmail.com>
2015-02-10 13:34:22 -0500

    fix number parse, substr is [,)

--- main.cpp
+++ main.cpp
@@ -261,7 +261,7 @@ int main(int args, char** argv) {
         }
       }
 
-      uint32_t node1 = uint32_t(atoi(line.substr(0, pos - 1).c_str()));
+      uint32_t node1 = uint32_t(atoi(line.substr(0, pos).c_str()));
       uint32_t node2 = uint32_t(atoi(line.substr(pos + 1, line.size()).c_str()));
       edgeList[node1].push_back(node2);
       edgeList[node2].push_back(node1);

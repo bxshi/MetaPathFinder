e34c881928387347e91c832e73fbd1e4ed9e3295
bxshi <baoxu.shi@gmail.com>
2015-02-10 00:44:36 -0500

    revert

--- main.cpp
+++ main.cpp
@@ -36,7 +36,7 @@ thread threadList[MAX_THREAD];
 //struct arg argList[MAX_THREAD];
 
 //vector<vector<vector<uint64_t>>> global_result;
-//bool **global_visited;
+bool **global_visited;
 
 vector<uint32_t> bfs_lookup(uint32_t src, vector<NodeType> &nodeDict,
     vector<vector<uint32_t>> &edgeDict, vector<NodeType> &mPath, bool *visited) {
@@ -141,33 +141,25 @@ string path_to_string(vector<NodeType> nodevec) {
 }
 
 
-void dfs_lookup(uint32_t root, uint32_t src, uint16_t mpath, uint8_t depth, uint16_t pid, vector<vector<uint64_t>>& resVec, bool *visited) {
-  if(src >= MAX_ID) {
-    cout << src << ">="<< MAX_ID<<endl;
-  }
-  try{
-    if(depth < max_depth) {
-      // new metapath from root to src
+void dfs_lookup(uint32_t root, uint32_t src, uint16_t mpath, uint8_t depth, uint16_t pid, vector<vector<uint64_t>>& resVec) {
+  if(depth < max_depth) {
+    // new metapath from root to src
 //    cout <<"root "<<root<<" src " << src<<" mpath "<<path_to_string(decode(mpath)) <<" depth " << depth <<" pid "<<pid<<endl;
-      visited[src] = true;
-      for (size_t i = 0; i < edgeList[src].size(); ++i) {
-        if (!visited[edgeList[src][i]]){ // not visited
-          uint16_t new_mpath = mpath + nodeList[edgeList[src][i]] * pow(NODETYPE_BASE, depth + 1);
-          if(nodeList[edgeList[src][i]] == Paper) { // a qualified endpoint, save it
-            uint64_t item = (uint64_t(root) << 32) + (edgeList[src][i]);
-            resVec[new_mpath].push_back(item);
+    global_visited[pid][src] = true;
+    for (size_t i = 0; i < edgeList[src].size(); ++i) {
+      if (!global_visited[pid][edgeList[src][i]]){ // not visited
+        uint16_t new_mpath = mpath + nodeList[edgeList[src][i]] * pow(NODETYPE_BASE, depth + 1);
+        if(nodeList[edgeList[src][i]] == Paper) { // a qualified endpoint, save it
+          uint64_t item = (uint64_t(root) << 32) + (edgeList[src][i]);
+          resVec[new_mpath].push_back(item);
 //          cout << "root " << (item >> 32) << " end " << ((item << 32) >> 32) << " path " << path_to_string(decode(new_mpath)) <<endl;
 //          cout << path_to_string(decode(new_mpath)) << " size " << global_result[pid][new_mpath].size();
-          }
-          dfs_lookup(root, edgeList[src][i], new_mpath, depth + 1, pid, resVec, visited);
         }
+        dfs_lookup(root, edgeList[src][i], new_mpath, depth + 1, pid, resVec);
       }
-      visited[src] = false;
     }
-  }catch (exception &e) {
-    cout <<e.what()<<endl;
+    global_visited[pid][src] = false;
   }
-
 }
 
 void newWorker(uint16_t pid) {
@@ -177,14 +169,13 @@ void newWorker(uint16_t pid) {
     // result vector for each thread
     vector<vector<uint64_t >> resVec;
     resVec.resize(1100);
-
-    cout <<"before init\n";
-    bool *visited = (bool *)malloc(MAX_ID * sizeof(bool));
-    cout <<"end init\n";
+    for (int k = 0; k < resVec.size(); ++k) {
+      resVec[k].reserve(1000);
+    }
 
     for (size_t i = pid; i < nodeList.size(); i += MAX_THREAD) {
       if (nodeList[i] == Paper && ((double) rand() / (double) RAND_MAX) <= PORTION) {
-        dfs_lookup(i, i, nodeList[i], 0, pid, resVec, visited);
+        dfs_lookup(i, i, nodeList[i], 0, pid, resVec);
 
         uint64_t cnt = 0;
         for (size_t j = 0; j < resVec[pid].size(); j++) {
@@ -217,13 +208,12 @@ void newWorker(uint16_t pid) {
 
     duration = chrono::high_resolution_clock::now() - start_time;
     cout << "save pid " << pid << " took " << chrono::duration_cast<chrono::microseconds>(duration).count() << endl;
+    free(global_visited[pid]);
     for (int s = 0; s < resVec.size(); ++s) {
       vector<uint64_t >().swap(resVec[s]);
     }
 
     vector<vector<uint64_t>>().swap(resVec);
-    free(visited);
-    cout << pid << " vector released\n";
   }
 }
 
@@ -334,6 +324,11 @@ int main(int args, char** argv) {
   } else {
     min_depth = uint8_t(atoi(argv[1]));
     max_depth = uint8_t(atoi(argv[2]));
+  }
+
+  global_visited = (bool **)malloc(sizeof(bool *) * MAX_THREAD);
+  for(size_t i = 0; i < MAX_THREAD; i++) {
+    global_visited[i] = (bool *) malloc(sizeof(bool) * MAX_ID);
   }
 
   srand(900207);

309ee28704d7286e6a6b3c204f2410f0822777c4
bxshi <baoxu.shi@gmail.com>
2015-02-09 21:27:17 -0500

    new_dfs version

--- main.cpp
+++ main.cpp
@@ -152,7 +152,7 @@ void dfs_lookup(uint32_t root, uint32_t src, uint16_t mpath, uint8_t depth, uint
         if(nodeList[edgeList[src][i]] == Paper) { // a qualified endpoint, save it
           uint64_t item = (uint64_t(root) << 32) + (edgeList[src][i]);
           global_result[pid][new_mpath].push_back(item);
-          cout << "root " << (item >> 32) << " end " << (item << 32) << " path " << path_to_string(decode(new_mpath)) <<endl;
+          cout << "root " << (item >> 32) << " end " << ((item << 32) >> 32) << " path " << path_to_string(decode(new_mpath)) <<endl;
         }
         dfs_lookup(root, edgeList[src][i], new_mpath, depth + 1, pid);
       }

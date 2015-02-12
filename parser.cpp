0e9a67fed07ab824ff41ee2041a0b5bf374c1f69
bxshi <baoxu.shi@gmail.com>
2015-02-12 11:09:39 -0500

    test load endpoints

--- parser.cpp
+++ parser.cpp
@@ -1,3 +1,4 @@
+#include <tclDecls.h>
 #include "mpf.h"
 
 vector<vector<uint32_t>> termList;
@@ -90,41 +91,56 @@ int main(int argc, char** argv) {
 
   auto start_time = chrono::high_resolution_clock::now();
 
-  std::ifstream input(argv[1]);
+  ifstream term_input(argv[1]);
 
   // Read words
 
   termList.resize(MAX_DOC_ID);
 
+  uint32_t max_term_id=0;
   string line;
-  while (getline(input, line)) {
+  while (getline(term_input, line)) {
     size_t pos = 0;
     uint32_t key = 0;
     for(size_t i = 0; i < line.size(); i++) { // last element is always comma
       if (line[i] == ',') {
-//        cout << "pos " << pos << "i " << i << " substr " << line.substr(pos, i);
         uint32_t val = atoi(line.substr(pos, i).c_str());
         if(pos == 0) {
           key = val;
         } else {
+          if (val > max_term_id) {
+            max_term_id = val;
+          }
           termList[key].push_back(val);
         }
         pos = i + 1;
       }
     }
     termList[key].shrink_to_fit();
-
-//    cout << "key is " << key;
-//    for (int j = 0; j < termList[key].size(); ++j) {
-//      cout << " " << termList[key][j] <<" ";
-//    }
-//
-//    cout << endl;
   }
+  term_input.close();
 
   auto duration = chrono::high_resolution_clock::now() - start_time;
-  cout << "Terms are loaded, took " << chrono::duration_cast<chrono::microseconds>(duration).count() << endl;
+  cout << "Terms are loaded, took " << chrono::duration_cast<chrono::microseconds>(duration).count() << "ms" << endl;
   cout << "#paper: " << termList.size() << endl;
+  cout << "Largest term id: " << max_term_id << endl;
+
+  ostringstream oss;
+  ostream bm25_output(argv[3]);
+
+  // Read endpoint pairs
+  ifstream edge_input(argv[2]);
+  while (getline(edge_input, line)) { // mpath src dst
+    istringstream is(line);
+    uint16_t mpathType;
+    uint32_t src;
+    uint32_t target;
+
+    is >> mpathType >> src >> target;
+    cout << mpathType << "  " << src << "  " << target << endl;
+    break;
+
+  }
 
   return 0;
 }
